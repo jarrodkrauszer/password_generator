9bc72282e731e98e1aad6ec4fa1c3b069efe5451
jarrodkrauszer <96881754+jarrodkrauszer@users.noreply.github.com>
2023-09-07 18:57:37 -0400

    started coding the javascript to generate the password

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var numbers = '0123456789';
+var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+var symbols = ' !"#$%&\'()*+,-./:;=<>?[\\]^_{}|~';
+
+function generatePassword(characters, passwordLength) {
+  let newPassword = "";
+
+  for (var i = 0; i < passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * characters.length);
+    newPassword += characters.substring(randomNumber, randomNumber +1);
+   }
+
+   return newPassword;
+}
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // var chars = '';
+  var chars = numbers + lowercase + lowercase.toUpperCase() + symbols;
+  var passwordLength = prompt('Password Length? Please choose between 8 and 128 characters');
 
+  passwordLength = parseInt(passwordLength);
+  console.log(typeof passwordLength);
+
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    var passwordLength = prompt('Please only choose between 8 and 128 characters');
+  }
+
+  var password  = generatePassword(chars, passwordLength);
+  var passwordText = document.querySelector("#password");
+  
   passwordText.value = password;
 
 }
